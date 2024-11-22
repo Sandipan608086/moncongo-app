cb77e97d40c89e783c18919cc91c0c131e7680eb
Streak Link <streaklink@gmail.com>
2024-11-22 18:20:23 +0530

    update

--- store/Api.jsx
+++
@@ -1,9 +0,0 @@
-const APIURL = {
-    BASEURL : "https://api.moncongo.com/v1/",
-    AUTH: "bbe88bce-cabe-4bf7-af92-43ecdb189f7b",
-    GOOGLE_MAP_ID: "",
-    GOOGLE_MAP_KEY: "AIzaSyDY-9GqKWZKl-Z7Hy8eVizmH22GBC8VIYQ",
-    COUNTRY_CODE: "https://api.moncongo.com/v1/CountryCodes.json"
-}
-
-export default APIURL;
